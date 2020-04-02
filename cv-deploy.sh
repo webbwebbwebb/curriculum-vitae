7c391e8e5eb2013b62ad9c880fa22516dd139515
mark.webb <mark@webbwebbwebb.co.uk>
2020-04-02 17:53:47 +0100

    Added deployment shell script

---
+++ cv-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+package_name=$1
+temp_directory=tmp_webbwebbwebb_deployment
+target_directory=webbwebbwebb.co.uk
+backup_directory=backup-webbwebbwebb-$(date +%Y-%m-%d-%H-%M-%S)
+
+echo Cleaning temp directory
+rm -rf $temp_directory
+mkdir $temp_directory
+
+echo Extracting "$package_name" to "$temp_directory"
+tar -C "$temp_directory" -xvf "$package_name"
+
+echo Copying "$target_directory" to "$backup_directory"
+rm -rf "$backup_directory"
+mkdir "$backup_directory"
+cp -pr "$target_directory"/** "$backup_directory"/
+
+echo Cleaning "$target_directory"
+find "$target_directory"  -maxdepth 1 ! -name .htaccess -type f -delete -print
+
+echo Updating "$target_directory" with contents of "$temp_directory"
+cp -r "$temp_directory"/dist/** "$target_directory"/

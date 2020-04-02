d5bf6d6c06843de5679cb499ed60f52decad6d1c
mark.webb <mark@webbwebbwebb.co.uk>
2020-04-02 10:41:14 +0100

    Added build script

---
+++ build.js
@@ -0,0 +1,34 @@
+const marked = require('marked');
+const fs = require('fs-extra');
+
+const CONTENT_PLACEHOLDER = '[!!CONTENT_HERE!!]';
+const SOURCE_DIRECTORY = 'src/';
+const OUTPUT_DIRECTORY = 'dest/';
+const OUTPUT_PATH = OUTPUT_DIRECTORY + 'index.html';
+
+fs.readFile(SOURCE_DIRECTORY + 'index.content.md', 'utf8', (err, data) => {
+    const content = marked(data);
+    fs.readFile(SOURCE_DIRECTORY + 'index.template.html', 'utf8', (err, template) => {
+        const output = template.replace(CONTENT_PLACEHOLDER, content);
+        fs.ensureDir(OUTPUT_DIRECTORY, err => {
+            if(err) {
+                return console.error(err);
+            }
+            fs.writeFile(OUTPUT_PATH, output, (err) => {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log('saved ' + OUTPUT_PATH);
+
+                fs.copy(SOURCE_DIRECTORY + 'assets', OUTPUT_DIRECTORY + 'assets', err => {
+                    if (err) {
+                        return console.error(err);
+                    } 
+                  
+                    console.log('copied ' + SOURCE_DIRECTORY + 'assets to ' + OUTPUT_DIRECTORY + 'assets');
+                });
+            });
+        });
+
+    });
+});

cdcb8eca4237b127f8e4930bb1afd2a1a91adb8d
mark.webb <mark@webbwebbwebb.co.uk>
2020-04-03 10:10:26 +0100

    Generate PDF copy

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+const pdf = require('html-pdf');
+const path = require('path');
 const marked = require('marked');
 const fs = require('fs-extra');
 
@@ -9,12 +11,12 @@ const OUTPUT_PATH = OUTPUT_DIRECTORY + 'index.html';
 fs.readFile(SOURCE_DIRECTORY + 'index.content.md', 'utf8', (err, data) => {
     const content = marked(data);
     fs.readFile(SOURCE_DIRECTORY + 'index.template.html', 'utf8', (err, template) => {
-        const output = template.replace(CONTENT_PLACEHOLDER, content);
+        const outputHTML = template.replace(CONTENT_PLACEHOLDER, content);
         fs.ensureDir(OUTPUT_DIRECTORY, err => {
             if(err) {
                 return console.error(err);
             }
-            fs.writeFile(OUTPUT_PATH, output, (err) => {
+            fs.writeFile(OUTPUT_PATH, outputHTML, (err) => {
                 if (err) {
                     return console.error(err);
                 }
@@ -26,6 +28,25 @@ fs.readFile(SOURCE_DIRECTORY + 'index.content.md', 'utf8', (err, data) => {
                     } 
                   
                     console.log('copied ' + SOURCE_DIRECTORY + 'assets to ' + OUTPUT_DIRECTORY + 'assets');
+                    
+                    var htmlWithAbsolutePaths = outputHTML.replace(/href="\/assets\//g, 'href="file:///' + path.join(__dirname, OUTPUT_DIRECTORY, 'assets/'));
+                    var pdfOptions = { 
+                        format: 'A4',
+                        orientation: 'portrait',
+                        border: {
+                            top: '1cm',
+                            right: '1cm',
+                            bottom: '1cm',
+                            left: '1cm'
+                          }
+                    };
+
+                    pdf.create(htmlWithAbsolutePaths, pdfOptions).toFile(OUTPUT_DIRECTORY + 'mjwcv.pdf', function(err, res) {
+                        if (err) { 
+                            return console.log(err);
+                        }
+                        console.log('saved PDF to ' + res.filename);
+                    });
                 });
             });
         });
